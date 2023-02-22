b9b7f61abe99fd3fc3378d54d57a676f5cc98354
Rob-Mundy <robertemundy@googlemail.com>
2023-02-22 15:34:29 +0000

    Add allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['HEROKU_HOSTNAME', 'localhost']
+ALLOWED_HOSTS = ['HEROKU_HOSTNAME', 'rm-django-todo-app.herokuapp.com' 'localhost']
 
 
 # Application definition
