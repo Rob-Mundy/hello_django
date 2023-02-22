7946b6a2fe4ea6df3336e675896a876832ed2361
Rob-Mundy <robertemundy@googlemail.com>
2023-02-22 15:04:59 +0000

    Amend env and settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-ALLOWED_HOSTS = ['rm-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = ['rm-django-todo-app.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse('postgres://igtoucsm:v68d6cQ6rkZIxwra14rp6HXf3Z7smUBk@manny.db.elephantsql.com/igtoucsm')
 }
 
 # Password validation
