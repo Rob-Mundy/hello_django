89be0fa29bd5f46d5c258c5a96ede0e703f63c06
Rob-Mundy <robertemundy@googlemail.com>
2023-02-22 12:14:30 +0000

    moved key to env

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--ug6ts_51b3apm0mucpt^#l*-y4j#d)_%hc0z&#o-&u63yf1rs'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['rm-django-todo-app.herokuapp.com']
 
