d5e94a3d8d8dbe68c2d957e867ab8aee100331a5
Rob-Mundy <robertemundy@googlemail.com>
2023-02-22 15:32:24 +0000

    set up environment variables and automatic deployments

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rm-django-todo-app.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['HEROKU_HOSTNAME', 'localhost']
 
 
 # Application definition
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse('postgres://igtoucsm:v68d6cQ6rkZIxwra14rp6HXf3Z7smUBk@manny.db.elephantsql.com/igtoucsm')
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
